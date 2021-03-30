7b9c382b360fe3302170a28bba6aabbc31bb9d08
Pazia Ramirez <pazia008@gmail.com>
2021-03-30 15:56:08 -0500

    a

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +10,17 @@ namespace DogGo.Models
     public class Owner
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
+
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
         public string Address { get; set; }
         public string Phone { get; set; }
