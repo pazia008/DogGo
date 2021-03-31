0ecfa94abf537ae2c4ab3dd49c07b33e6700025d
Pazia Ramirez <pazia008@gmail.com>
2021-03-31 15:07:44 -0500

    d

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace DogGo.Models
     public class Dog
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
         public int OwnerId { get; set; }
         public string Breed { get; set; }
