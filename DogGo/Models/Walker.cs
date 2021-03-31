6cdff8f7249a8dde590cfdc506ca627fe05f2320
Pazia Ramirez <pazia008@gmail.com>
2021-03-31 15:40:47 -0500

    added validation attributes

--- DogGo/Models/Walker.cs
+++ DogGo/Models/Walker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace DogGo.Models
     public class Walker
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
         public int NeighborhoodId { get; set; }
         public string ImageUrl { get; set; }
